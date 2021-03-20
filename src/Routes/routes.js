0e1abd81ba3602af974eb382e57c9331c1a6ecf8
Osée Botendju <oseebotendju@gmail.com>
2021-03-20 10:58:01 +0100

    Ajout UserList dans le module Routes

--- src/Routes/routes.js
+++ src/Routes/routes.js
@@ -1,10 +1,15 @@
-import { UserCreate } from "../Domain/User/User"
+import { UserCreate, UserList } from "../Domain/User/User"
 
 const routes = [
     {
         path: "/users/create",
         title: "Create User",
         component: UserCreate
+    },
+    {
+        path: "/users",
+        title: "List of Users",
+        component : UserList
     }
 ]
 
